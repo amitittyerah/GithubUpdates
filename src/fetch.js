553cc0f1bbf9e872325ffbfafbb1f8c8b1d6a54f
Amit Benjamin <admin@devscoop.com>
2014-06-22 13:34:22 +1000

    Silly me, hardcoded my name

--- src/fetch.js
+++ src/fetch.js
@@ -27,57 +27,64 @@ function build_commit_url (url) {
 	return url.replace('/api.', '/').replace('/repos/', '/');
 }
 
-$.get('https://api.github.com/users/devscoop/received_events', function (data) {
-	var append = '';
-	for(var i = 0 ; i < data.length ; i++)
+chrome.storage.sync.get(function(data) {
+	if(data.username)
 	{
-		var activity = data[i];
-		if (activity.type === 'IssueCommentEvent')
+		$.get('https://api.github.com/users/' + data.username + '/received_events', function (data) {
+		var append = '';
+		for(var i = 0 ; i < data.length ; i++)
 		{
-			append += build_block(actor_url(activity.actor) 
-				+ ' commented on ' 
-				+ repo_url(activity.repo)
-				+ get_date(activity));
+			var activity = data[i];
+			if (activity.type === 'IssueCommentEvent')
+			{
+				append += build_block(actor_url(activity.actor) 
+					+ ' commented on ' 
+					+ repo_url(activity.repo)
+					+ get_date(activity));
+			}
+			else if (activity.type === 'WatchEvent')
+			{
+				append += build_block(actor_url(activity.actor) 
+					+ ' watching ' 
+					+ repo_url(activity.repo)
+					+ get_date(activity));
+			}
+			else if (activity.type === 'PullRequestEvent')
+			{
+				append += build_block(actor_url(activity.actor) 
+					+ ' pulled ' 
+					+ repo_url(activity.repo)
+					+ get_date(activity));
+			}
+			else if (activity.type === 'ForkEvents')
+			{
+				append += build_block(actor_url(activity.actor) 
+					+ ' forked ' 
+					+ repo_url(activity.repo)
+					+ get_date(activity));
+			}
+			else if (activity.type === 'PushEvent')
+			{
+				append += build_block(actor_url(activity.actor) 
+					+ ' pushed to ' 
+					+ repo_url(activity.repo) 
+					+ '(<a href="' + build_commit_url(activity.payload.commits[0].url) + '">Commit</a>)'
+					+ get_date(activity));
+			}
+			else if (activity.type === 'CreateEvent')
+			{
+				append += build_block(actor_url(activity.actor) 
+					+ ' created ' 
+					+ repo_url(activity.repo) 
+					+ get_date(activity));
+			}
 		}
-		else if (activity.type === 'WatchEvent')
-		{
-			append += build_block(actor_url(activity.actor) 
-				+ ' watching ' 
-				+ repo_url(activity.repo)
-				+ get_date(activity));
-		}
-		else if (activity.type === 'PullRequestEvent')
-		{
-			append += build_block(actor_url(activity.actor) 
-				+ ' pulled ' 
-				+ repo_url(activity.repo)
-				+ get_date(activity));
-		}
-		else if (activity.type === 'ForkEvents')
-		{
-			append += build_block(actor_url(activity.actor) 
-				+ ' forked ' 
-				+ repo_url(activity.repo)
-				+ get_date(activity));
-		}
-		else if (activity.type === 'PushEvent')
-		{
-			append += build_block(actor_url(activity.actor) 
-				+ ' pushed to ' 
-				+ repo_url(activity.repo) 
-				+ '(<a href="' + build_commit_url(activity.payload.commits[0].url) + '">Commit</a>)'
-				+ get_date(activity));
-		}
-		else if (activity.type === 'CreateEvent')
-		{
-			append += build_block(actor_url(activity.actor) 
-				+ ' created ' 
-				+ repo_url(activity.repo) 
-				+ get_date(activity));
-		}
+		$('body').append(append);
+		$('a').unbind('click').click(function(item) {
+	     	chrome.tabs.create({url: $(item.target).attr('href')});
+	  });
+	});
 	}
-	$('body').append(append);
-	$('a').unbind('click').click(function(item) {
-     	chrome.tabs.create({url: $(item.target).attr('href')});
-  });
 });
+
+
